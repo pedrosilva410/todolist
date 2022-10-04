eb3a75d84ac01dfeb8e76d625332cbe12f95cde6
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2022-10-04 23:56:04 +0200

    create li

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ document.getElementById('addItem').addEventListener('click', () => addItem())
 
 function addItem() {
 	value = document.getElementById('userInput').value
-
+	const newLi = document.createElement("li");
+	const newP = document.createElement("p");
+	const newButton = document.createElement("button");
 }
