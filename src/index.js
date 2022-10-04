dbdfdc610ea0ade1cafe078c345094ed7646671c
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2022-10-05 00:27:45 +0200

    add and remove items from todo list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-document.getElementById('addItem').addEventListener('click', () => addItem())
+document.getElementById('addItem').addEventListener('click', () => addItem());
 
 
 function addItem() {
-	value = document.getElementById('userInput').value
-	const newLi = document.createElement("li");
-	const newP = document.createElement("p");
-	const newButton = document.createElement("button");
+	const newLi = document.createElement('li');
+	const newP = document.createElement('p');
+	const newButton = document.createElement('button');
+	const list = document.getElementById('list');
+	const input = document.getElementById('userInput').value;
+
+	newP.innerHTML = input;
+	newButton.innerHTML = 'x';
+
+	document.getElementById('userInput').value = '';
+
+	newButton.addEventListener('click', () => newLi.remove());
+
+	newLi.classList.add('listItem');
+	newP.classList.add('itemText');
+	newButton.classList.add('removeItem');
+
+	newLi.append(newP);
+	newLi.append(newButton);
+	list.append(newLi);
 }
