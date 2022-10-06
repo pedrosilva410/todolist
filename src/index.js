5625320fdefa81954889dc8f4d467cf8e6356be4
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2022-10-06 21:17:32 +0200

    fixed empty input add

--- src/index.js
+++ src/index.js
@@ -8,18 +8,22 @@ function addItem() {
 	const newButton = document.createElement('button');
 	const input = document.getElementById('userInput');
 
-	newP.innerHTML = input.value;
-	newButton.innerHTML = 'x';
+	if (input.value) {
+		newP.innerHTML = input.value;
+		newButton.innerHTML = 'x';
 
-	input.value = '';
+		input.value = '';
 
-	newButton.addEventListener('click', () => newLi.remove());
+		newButton.addEventListener('click', () => newLi.remove());
 
-	newLi.classList.add('listItem');
-	newP.classList.add('itemText');
-	newButton.classList.add('removeItem');
+		newLi.classList.add('listItem');
+		newP.classList.add('itemText');
+		newButton.classList.add('removeItem');
 
-	newLi.append(newP);
-	newLi.append(newButton);
-	list.append(newLi);
+		newLi.append(newP);
+		newLi.append(newButton);
+		list.append(newLi);
+	} else {
+		alert('Write something, unless you feel like doing nothing :)')
+	}
 }
