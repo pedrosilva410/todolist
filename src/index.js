08842fe38315404360d72f59ac12120843081137
Pedro <pedrogdsilva410@gmail.com>
2023-10-25 21:38:48 +0200

    tried to implement todo list add item
    needs work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const date = new Date()
 const todayDate = date.toLocaleDateString('de-de');
-document.getElementById("todayDate").innerHTML = todayDate;
+document.getElementById('todayDate').innerHTML = todayDate;
 console.log(todayDate);
+
+const userInput = document.getElementById('userInput');
+const addButton = document.getElementById('addButton');
+const todoList = document.getElementById('todoList')
+
+addButton.addEventListener(click, addItem)
+
+function addItem() {
+	const newItem = document.createElement('li');
+	const newText = document.createElement('p')
+	const newInput = userInput.innerHTML()
+
+	newText.appendChild(newInput);
+	newItem.appendChild(newText);
+	todoList.appendChild(newItem);
+}
