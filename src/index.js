7af62e105823da3b0e0a0a1c6df6da3673742ccf
Pedro <pedrogdsilva410@gmail.com>
2023-10-25 21:52:27 +0200

    fix: date

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const date = new Date()
-const todayDate = date.toLocaleDateString('de-de');
+const date = new Date();
+const todayDate = date.toLocaleDateString();
 document.getElementById('todayDate').innerHTML = todayDate;
 console.log(todayDate);
 
 const userInput = document.getElementById('userInput');
 const addButton = document.getElementById('addButton');
-const todoList = document.getElementById('todoList')
+const todoList = document.getElementById('todoList');
 
-addButton.addEventListener(click, addItem)
+addButton.addEventListener(click, addItem);
 
 function addItem() {
 	const newItem = document.createElement('li');
-	const newText = document.createElement('p')
-	const newInput = userInput.innerHTML()
+	const newText = document.createElement('p');
+	const newInput = userInput.innerHTML();
 
 	newText.appendChild(newInput);
 	newItem.appendChild(newText);
