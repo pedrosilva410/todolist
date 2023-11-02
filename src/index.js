30efe2a80f29066dcb16b93c457fd2d603756ace
Pedro <pedrogdsilva410@gmail.com>
2023-11-02 13:07:10 +0100

    add; check and uncheck todo list item

--- src/index.js
+++ src/index.js
@@ -9,12 +9,24 @@ const todoList = document.getElementById('todoList');
 
 function addItem() {
 	if (userInput.value === '') {
-		alert('You really got nothing to do?')
+		alert('You really got nothing to do?');
 	}
 	else {
 		const li = document.createElement('li');
 		li.innerHTML = userInput.value;
-		todoList.appendChild(li)
+		todoList.appendChild(li);
+		let span = document.createElement('span');
+		span.innerHTML = '\u00d7';
+		li.appendChild(span);
 		userInput.value = ''
 	}
 }
+
+todoList.addEventListener('click', function (e) {
+	if (e.target.tagName === 'LI') {
+		e.target.classList.toggle('checked');
+	}
+	else if (e.target.tagName === 'SPAN') {
+		e.target.parentElement.remove();
+	}
+}, false);
